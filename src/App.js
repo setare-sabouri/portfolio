03a69cc1bf083b49de0b32c7e07064c24725f127
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-05-14 16:43:20 +0200

    projects component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Navbar } from "./components/navbar/Navbar";
 import { About } from "./components/main/about";
 import { Skills } from "./components/skills/skills";
 import { DownloadButton } from "./components/Resume/resume";
+import { Projects } from "./components/Projects/Projects";
 function App() {
   const cvURL = process.env.PUBLIC_URL + "/CV.pdf";
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="container-fluid h-100 " >
         <Navbar />
         <About />
+        <Projects />
         <Skills />
         <DownloadButton fileURL={cvURL} />
       </div >
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./projects.css"
+export const Projects = () => {
+
+    return (
+        <>khali</>
+    );
+};
