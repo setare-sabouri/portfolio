584719d01279d93466c4977e348d87f90fec46de
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-05-24 00:33:13 +0200

    github seperated fill color

--- src/components/Projects/DataProjects.js
+++ src/components/Projects/DataProjects.js
@@ -5,7 +5,10 @@ const projects = [
         description: "Shopping / Nike Shoes",
         previewImage: "/projectImages/Nike-Shoes.png",
         url: "https://react-nike-website.vercel.app/",
-        githubUrl: "https://github.com/setare-sabouri/React-Nike-Website"
+        githubUrl: "https://github.com/setare-sabouri/React-Nike-Website",
+        githubColor: "#1b00cd"
+
+
     },
     {
         id: 2,
@@ -13,21 +16,35 @@ const projects = [
         description: "",
         previewImage: "/projectImages/Currency.png",
         url: "https://currencyconvertfrankfurter.netlify.app/",
-        githubUrl: "https://github.com/setare-sabouri/Currency-Api"
+        githubUrl: "https://github.com/setare-sabouri/Currency-Api",
+        githubColor: "#1dbc04"
+
     },
     {
         id: 3,
         title: "Björnbacka",
         previewImage: "/projectImages/Björnbacka.png",
         url: "https://bjornbacka-9ffo.vercel.app/",
-        githubUrl: "https://github.com/code-collaborate-fed24/bjornbacka"
+        githubUrl: "https://github.com/code-collaborate-fed24/bjornbacka",
+        githubColor: "#b75500"
+
     },
     {
         id: 4,
         title: "Pokemon Memory game",
         previewImage: "/projectImages/memorygame.png",
-        url: "https://bjornbacka-9ffo.vercel.app/",
-        githubUrl: "https://github.com/setare-sabouri/pokemonBoard"
+        url: "https://pokemonboardgame.netlify.app/",
+        githubUrl: "https://github.com/setare-sabouri/pokemonBoard",
+        githubColor: "#e6f300"
+
+    },
+    {
+        id: 4,
+        title: "Hangman Game",
+        previewImage: "/projectImages/Hangman.png",
+        url: "https://hangmanwithjavascript.netlify.app/",
+        githubUrl: "https://github.com/setare-sabouri/Hangman",
+        githubColor: "#f64421"
     },
 
 ];
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -24,7 +24,7 @@ function Projects() {
                             <h3>{project.title}</h3>
                             <p>{project.description}</p>
                             <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
-                                <FaGithub className="github-icon" />
+                                <FaGithub className="github-icon" style={{ fill: project.githubColor }} />
                             </a>
                         </Carousel.Caption>
                     </Carousel.Item>
