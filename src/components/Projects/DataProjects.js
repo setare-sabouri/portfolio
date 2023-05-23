1ebce2b95e0353678406f7db4b636183131c22f5
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-05-24 00:20:38 +0200

    github links updated

--- src/components/Projects/DataProjects.js
+++ src/components/Projects/DataProjects.js
@@ -4,28 +4,32 @@ const projects = [
         title: "shoes Shopping Web",
         description: "Shopping / Nike Shoes",
         previewImage: "/projectImages/Nike-Shoes.png",
-        fullImage: "project1-full.jpg",
         url: "https://react-nike-website.vercel.app/",
-        githubUrl: "https://react-nike-website.vercel.app/"
+        githubUrl: "https://github.com/setare-sabouri/React-Nike-Website"
     },
     {
         id: 2,
         title: "Currency Exchange",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        description: "",
         previewImage: "/projectImages/Currency.png",
-        fullImage: "project2-full.jpg",
         url: "https://currencyconvertfrankfurter.netlify.app/",
-        githubUrl: "https://react-nike-website.vercel.app/"
+        githubUrl: "https://github.com/setare-sabouri/Currency-Api"
     },
     {
         id: 3,
         title: "Björnbacka",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         previewImage: "/projectImages/Björnbacka.png",
-        fullImage: "project3-full.jpg",
         url: "https://bjornbacka-9ffo.vercel.app/",
-        githubUrl: "https://react-nike-website.vercel.app/"
-    }
+        githubUrl: "https://github.com/code-collaborate-fed24/bjornbacka"
+    },
+    {
+        id: 4,
+        title: "Pokemon Memory game",
+        previewImage: "/projectImages/memorygame.png",
+        url: "https://bjornbacka-9ffo.vercel.app/",
+        githubUrl: "https://github.com/setare-sabouri/pokemonBoard"
+    },
+
 ];
 
 export default projects
