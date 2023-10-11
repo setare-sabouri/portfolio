3aa9144acf35f6752275791ba388e2ddd4fc0e9a
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-10-11 13:14:42 +0200

    Update SkillsData.js

--- src/components/skills/SkillsData.js
+++ src/components/skills/SkillsData.js
@@ -1,7 +1,10 @@
 const skillsData = [
     { name: "HTML", percentage: 90 },
     { name: "CSS", percentage: 85 },
+    { name: "Sass", percentage: 85 },
     { name: "JavaScript", percentage: 80 },
+    { name: "Typescript", percentage: 60 },
+    { name: "Next.js", percentage: 60 },
     { name: "React", percentage: 80 },
     { name: "Git / Github", percentage: 90 },
     { name: "Bootstrap", percentage: 95 },
@@ -10,8 +13,9 @@ const skillsData = [
     { name: "DOM Manipulation", percentage: 80 },
     { name: "Responsive Design", percentage: 75 },
     { name: "Debugging", percentage: 90 },
-    { name: "Typescript", percentage: 60 },
-    { name: "Next.js", percentage: 60 },
+    { name: "GSAP", percentage: 30 },
+    { name: "Lenis", percentage: 30 },
+
 
 
 ];
