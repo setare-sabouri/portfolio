69fa9e9f04cd4582a03e062d263d8c3d1b296e47
setareh <100029656+setare-sabouri@users.noreply.github.com>
2024-11-27 17:34:29 +0000

    fallback

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,22 +1,20 @@
 // src/pages/LandingPage.js
-import React, { Suspense } from 'react';
-import { IntroText } from '../components/IntroText/IntroText';
-import Button from '../models/Button';
-import { Canvas } from '@react-three/fiber';
-import { Environment } from '@react-three/drei';
+import React, { Suspense } from "react";
+import { IntroText } from "../components/IntroText/IntroText";
+import Button from "../models/Button";
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
 
 function LandingPage() {
-
-    return (
-        <Canvas >
-            <Suspense fallback={<div>Sorry no WebGL supported!</div>}>
-                <Environment preset="sunset" background />
-                <IntroText />
-                <Button />
-            </Suspense>
-        </Canvas>
-
-    );
+  return (
+    <Canvas>
+      <Suspense fallback={null}>
+        <Environment preset="sunset" background />
+        <IntroText />
+        <Button />
+      </Suspense>
+    </Canvas>
+  );
 }
 
 export default LandingPage;
