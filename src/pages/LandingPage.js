67685c833b0d9da3b100399c19bc1e8e9fa6bb4d
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-12-15 19:06:07 +0100

    fixes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,4 @@
 // src/pages/LandingPage.js
-<<<<<<< HEAD
 import React, { Suspense } from 'react';
 import { Intro } from '../components/Intro/Intro';
 import { Canvas } from '@react-three/fiber';
@@ -16,24 +15,6 @@ function LandingPage() {
         </Canvas>
 
     );
-=======
-import React, { Suspense } from "react";
-import { IntroText } from "../components/IntroText/IntroText";
-import Button from "../models/Button";
-import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
-
-function LandingPage() {
-  return (
-    <Canvas>
-      <Suspense fallback={null}>
-        <Environment preset="sunset" background />
-        <IntroText />
-        <Button />
-      </Suspense>
-    </Canvas>
-  );
->>>>>>> 69fa9e9f04cd4582a03e062d263d8c3d1b296e47
 }
 
 export default LandingPage;
