1d74b69d69284c102ea9f78bd0867efeb460d583
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-12-15 23:28:30 +0100

    new text

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -3,12 +3,13 @@ import TextGeo from '../../models/TextGeo.js';
 import './Intro.scss';
 import { Perf } from 'r3f-perf';
 import Button from '../../models/Button.js';
-
+import Donut from '../../models/Donut.js';
 export const Intro = () => {
   return (
     <>
       <Perf position="top-left" />
       <TextGeo />
+      {/* <Donut /> */}
       <Button />
     </>
   );
--- src/components/main/about.js
+++ src/components/main/about.js
@@ -14,11 +14,20 @@ export const About = () => {
             <div class="info-wrapper col-sm-6 col-md-6 col-lg-6 col-xl-6 px-4">
                 <div className="right-head image-blur"></div>
                 <div className='right-head content-on-blur'>
-                    <h1 >Hi, i'm Setare</h1>
+                    <h1 >Welcome to my portfolio!</h1>
                     <p class="">
-                        I love to create, learn, develop, and teach continuously,<br />
-                        I'm curious about exploring the connection between my interests and the world around me,<br />
-                        When I'm not coding, you can usually find me painting, making art, hiking, biking, or learning new fields !
+
+                        Currently under development, <br />
+                        this space reflects my journey as a front-end developer, where creativity and learning go hand in hand.<br />
+                        I’m passionate about crafting engaging and intuitive user experiences, and I take pride in sharing the knowledge I’ve gained along the way.
+
+                        I’m always curious about the intersection of technology and my personal interests, which inspires me to think outside the box.<br />
+                        Front-end development allows me to bring ideas to life through design and functionality, and it’s truly what I love doing.
+
+                        When I’m not coding, you’ll often find me painting, exploring art, hiking, biking, or discovering fun new life hacks.<br />
+                        <h5> Creativity fuels everything I do — whether it’s on a canvas or in code.</h5>
+
+
                     </p>
                 </div>
 
