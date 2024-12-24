aa7dbae8d7118ecfb4fd6f9f6c1a43ae5644d74b
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-12-24 19:54:45 +0100

    SRGB

--- src/hooks/useMatcapMaterial.js
+++ src/hooks/useMatcapMaterial.js
@@ -2,18 +2,17 @@
 import { useMatcapTexture } from "@react-three/drei";
 import * as THREE from 'three'
 import { useMemo } from "react";
-
-
 export const useTextMatCap = () => {
     const [matCapTextue] = useMatcapTexture('9F1A27_F1AF7F_CD5845_D08441', 256)
 
     const material = useMemo(() => {
+        matCapTextue.colorSpace = THREE.SRGBColorSpace;
+        matCapTextue.needsUpdate = true;
+
         const material = new THREE.MeshMatcapMaterial();
         material.matcap = matCapTextue;
         material.needsUpdate = true;
-
         return material;
-
     }, [matCapTextue]);
 
     return material;
