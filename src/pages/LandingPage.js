56f700921ce4670f98bd0540f87bcc6754501d07
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2025-01-13 01:51:46 +0100

    canvas responsiveness

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,18 +5,28 @@ import { Canvas } from '@react-three/fiber';
 import { Environment } from '@react-three/drei';
 import { OrbitControls } from '@react-three/drei';
 import Loading from '../components/Landing page/Loading/Loading';
+import { useMediaQuery } from 'react-responsive';
+
 function LandingPage() {
+    // Detect if the screen width is less than or equal to 768px (mobile)
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
     return (
         <Suspense fallback={<Loading />}>
-            <Canvas >
-                <OrbitControls />
+            <Canvas
+                camera={{
+                    position: isMobile ? [0, 0, 12] : [0, 0.5, 10],  // Zoom out on mobile
+                    fov: isMobile ? 80 : 60,                    // Wider view on mobile
+                }}
+            >
+                <OrbitControls
+                    maxDistance={30}
+                />
+
                 <Environment preset="city" />
                 <Intro />
             </Canvas>
         </Suspense>
-
-
     );
 }
 
